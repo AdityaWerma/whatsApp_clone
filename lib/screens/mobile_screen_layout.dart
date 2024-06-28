b8344cc4e97d93d5ff056ed1c128046976810204
aditya kumar <akv.small.api@gmail.com>
2024-06-28 21:30:32 +0530

    Designed mobile screen layout

---
+++ lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
+
+import '../colors.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          centerTitle: false,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.grey,
+            ),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.search, color: Colors.grey)),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert, color: Colors.grey)),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            indicatorSize: TabBarIndicatorSize.tab,
+            labelColor: tabColor,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(
+                text: 'CHATS',
+              ),
+              Tab(
+                text: 'STATUS',
+              ),
+              Tab(
+                text: 'CALLS',
+              ),
+            ],
+          ),
+        ),
+        body: const ContactsList(),
+      ),
+    );
+  }
+}
